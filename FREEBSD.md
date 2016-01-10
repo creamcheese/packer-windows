23cf9d0f999dfaf51f7e1cf88e917b688323f78a
Matthew <matthew@proxbox.co.uk>
2016-01-10 02:27:11 +0000

    Folder structure change. Added initial FreeBSD (for firewall/router) template.

---
+++ FREEBSD.md
@@ -0,0 +1,39 @@
+# FreeBSD Packer template
+
+Prerequisites for using either the 10.1-RELEASE or -CURRENT boxes:
+
+ * Install [Vagrant](https://www.vagrantup.com)
+ * Install [Packer](https://www.packer.io/)
+ * Clone this repo onto your machine
+
+To create a vagrant box for FreeBSD 10.1-RELEASE or -CURRENT (r276981):
+
+ * Build the Vagrant box with: `packer build template-10.1.json`
+ * Wait.
+ * Add the appropriate Vagrant box for your system.  For example, on VMware:
+   `vagrant box add --name FreeBSD-10.1 FreeBSD-10.1-RELEASE-vmware.box`
+
+To create a vagrant box for -CURRENT:
+
+ * Build using `./automatic-current.sh`
+   * This will autodetect the latest -CURRENT snapshot, pull it, and apply
+     the recipe.  NOTE: THIS MAY FAIL ON NEWER BUILDS.
+ * Add the appropriate Vagrant box for your system.  For example, on VirtualBox:
+   `vagrant box add --name FreeBSD-CURRENT-r276981 FreeBSD-CURRENT-r276981-virtualbox.box`
+
+To use any Vagrant box:
+
+ * Initialize vagrant using: `vagrant init --minimal <box name>`
+ * Start the Vagrant VM: `vagrant up`
+ * Connect to the VM: `vagrant ssh`
+ * Hack away
+
+Notes:
+
+ * The VM is set to have 1024MB of RAM and a 20GB drive
+ * When bringing vagrant boxes up for the first time, you will
+   need sudo privileges to add entries in /etc/exports to allow
+   vagrant to mount /vagrant in the guest.  See 
+   [Vagrant NFS synced folders](https://docs.vagrantup.com/v2/synced-folders/nfs.html)
+ * Currently, auto install from ISO is via packer boot commands.  This is fragile, need to 
+   look at using /etc/installerscript for auto unattended install of initial OS.
---
+++ scripts/bsd/postinstall.sh
@@ -0,0 +1,71 @@
+#!/bin/sh
+
+echo 'Running post-install..'
+
+echo 'Setting up pkg'
+if [ ! -f /usr/local/sbin/pkg ]; then
+	ASSUME_ALWAYS_YES=yes pkg bootstrap
+fi
+
+echo 'Setting up VM Tools..'
+if [ "$PACKER_BUILDER_TYPE" = 'vmware-iso' ]; then
+	pkg install -y open-vm-tools-nox11
+	echo 'vmware_guest_vmblock_enable="YES"' >> /etc/rc.conf
+	echo 'vmware_guest_vmhgfs_enable="YES"' >> /etc/rc.conf
+	echo 'vmware_guest_vmmemctl_enable="YES"' >> /etc/rc.conf
+	echo 'vmware_guest_vmxnet_enable="YES"' >> /etc/rc.conf
+	echo 'vmware_guestd_enable="YES"' >> /etc/rc.conf
+elif [ "$PACKER_BUILDER_TYPE" = 'virtualbox-iso' ]; then
+	pkg install -y virtualbox-ose-additions
+	echo 'ifconfig_em1="inet 10.6.66.42 netmask 255.255.255.0"' >> /etc/rc.conf
+	echo 'vboxguest_enable="YES"' >> /etc/rc.conf
+	echo 'vboxservice_enable="YES"' >> /etc/rc.conf
+else
+	echo 'Unknown type of VM, not installing tools..'
+fi
+
+echo
+echo 'Installing bash for convenience..'
+pkg install bash
+
+echo
+echo 'Setting up fdesc mount..'
+mount -t fdescfs fdesc /dev/fd
+echo "fdesc  /dev/fd  fdescfs   rw  0  0" >> /etc/fstab
+
+echo
+echo 'Setting up sudo..'
+pkg install -y sudo
+echo 'vagrant ALL=(ALL) NOPASSWD: ALL' > /usr/local/etc/sudoers.d/vagrant
+
+echo
+echo 'Setting up the vagrant ssh keys'
+mkdir ~vagrant/.ssh
+chmod 700 ~vagrant/.ssh
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key" > ~vagrant/.ssh/authorized_keys
+chown -R vagrant ~vagrant/.ssh
+chmod 600 ~vagrant/.ssh/authorized_keys
+
+echo
+echo 'Changing roots shell back'
+chsh -s tcsh root
+
+# This causes a hang on shutdown that we cannot automatically recover from
+#echo 'Patching FreeBSD..'
+#freebsd-update fetch install > /dev/null
+
+echo 'Virtualbox additions'
+pkg install emulators/virtualbox-ose-additions
+echo 'vboxguest_enable="YES"' >> /etc/rc.conf
+echo 'vboxservice_enable="YES"' >> /etc/rc.conf
+
+echo 'Remaining packages..'
+pkg install dnsmasq
+pkg install python
+# Utilities
+pkg install bind-tools
+pkg install curl
+pkg install nano
+
+echo
+echo 'Post-install complete.'
